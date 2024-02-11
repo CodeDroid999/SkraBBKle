93735bfad1037b21039c6b88db0bec7f540ffffa
CodeDroid99 <johnmwega2002@gmail.com>
2024-02-11 12:18:08 +0300

    first run

--- src/pij/main/Main.java
+++ src/pij/main/Main.java
@@ -1,5 +1,9 @@
 package pij.main;
 
 public class Main {
-
+    public static void main(String[] args) {
+        // Code for initiating the game
+        System.out.println("Welcome to SkraBBKle!");
+        // Add code for initiating the game here
+    }
 }
