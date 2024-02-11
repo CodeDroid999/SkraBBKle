b30448f247e99b20d638f2495b0ec5acdf20f3c8
CodeDroid99 <johnmwega2002@gmail.com>
2024-02-11 13:56:48 +0300

    ffffffffffffffffff

---
+++ src/pij/main/Position.java
@@ -0,0 +1,24 @@
+package pij.main;
+
+public class Position {
+    private int row;
+    private int column;
+
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + row + "," + column + "]";
+    }
+}
